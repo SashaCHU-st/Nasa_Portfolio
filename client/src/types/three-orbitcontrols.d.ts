3a064e72a652fb99f2db72f5a1b1e78b71fbf02d
SashaCHU-st <alexandra.churina@gmail.com>
2025-09-02 12:25:12 +0300

    deploying to vercel

---
+++ client/src/types/three-orbitcontrols.d.ts
@@ -0,0 +1,8 @@
+declare module 'three/examples/jsm/controls/OrbitControls' {
+  import { Camera, EventDispatcher, MOUSE, TOUCH } from 'three';
+  export class OrbitControls extends EventDispatcher {
+    constructor(object: Camera, domElement?: HTMLElement);
+    enableDamping: boolean;
+    update(): void;
+  }
+}
