22e6fe21a6119b939139113cdc404d79a45f9cae
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-09-03 10:52:04 +0300

    feat: Added route to get all users

--- server/controllers/UserControllers.ts
+++ server/controllers/UserControllers.ts
@@ -16,3 +16,22 @@ export async function me(req: FastifyRequest, reply: FastifyReply) {
     return reply.code(500).send({ message: "Something went wrong" });
   }
 }
+
+
+export async function users(req: FastifyRequest, reply: FastifyReply) {
+  try
+  {
+    const userId = await authorisation(req);
+    
+    const allUsers = await pool.query(`SELECT name, email FROM users WHERE id != $1`,[userId])
+    
+    console.log("USERS=>", allUsers.rows)
+
+    return reply.code(200).send({ allUsers:allUsers.rows });
+  }catch (err: any) {
+    console.error("Database error:", err.message);
+    return reply.code(500).send({ message: "Something went wrong" });
+  }
+
+
+}
--- server/routes/UsersRoutes.ts
+++ server/routes/UsersRoutes.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from "fastify";
-import {me} from "../controllers/UserControllers"
+import {me, users} from "../controllers/UserControllers"
 export async function UsersRoutes(app: FastifyInstance) {
+
+    console.log("WE in USERS")
     app.get("/me",me);
+        app.get("/users",users);
 }
