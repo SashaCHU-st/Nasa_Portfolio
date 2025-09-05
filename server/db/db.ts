3674402bdb61cb6bcc4be38cb13d09d7f7199043
SashaCHU-st <alexandra.churina@gmail.com>
2025-09-05 14:18:50 +0300

    deploying

--- server/db/db.ts
+++ server/db/db.ts
@@ -5,8 +5,11 @@ dotenv.config();
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-
 pool.connect()
+  .then(() => console.log("✅ DB connected"))
   .catch((err) => console.error('❌ DB connection error:', err));
