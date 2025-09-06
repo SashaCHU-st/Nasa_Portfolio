6ff2da2ff37bcda565b54b9e4ee414e687bd2200
SashaCHU-st <alexandra.churina@gmail.com>
2025-09-06 16:16:34 +0300

    fix: responsiveness for deployment

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,18 +1,18 @@
 import { useParams } from "react-router-dom";
-import Neptune from "../backgrounds/Neptune"
+import Neptune from "../backgrounds/Neptune";
 import ProfileComponent from "../components/ProfileComponent";
 
 const Profile = () => {
   const { id } = useParams<{ id: string }>();
+
   return (
-    <div className="relative min-h-screen w-full flex justify-center items-start overflow-x-hidden">
-      <Neptune/>
-      <div className="absolute top-1/25 w-11/12 max-w-[95%] px-4">
-          <ProfileComponent
-          id={id!}/>
+    <div className="relative min-h-screen w-full flex justify-center items-start overflow-x-hidden bg-black">
+      <Neptune />
+      <div className="absolute  left-1/2 transform -translate-x-1/2 w-11/12 sm:w-9/12 md:w-7/12 max-w-[95%] px-4">
+        <ProfileComponent id={id!} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -4,9 +4,9 @@ import SearchInput from "../components/SearchInput";
 const Search = () => {
   return (
     <div className="relative min-h-screen w-full flex justify-center items-start overflow-x-hidden bg-black">
-      <Sun  /> 
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-7/12 sm:max-w-[85%] px-2">
-        <SearchInput /> 
+      <Sun />
+      <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 w-10/12 sm:w-8/12 md:w-6/12 max-w-[90%] px-2">
+        <SearchInput />
       </div>
     </div>
   );
