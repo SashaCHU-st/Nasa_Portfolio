5fd3f9c09ea9821244fa38cab6bea0f7d1b549b8
SashaCHU-st <alexandra.churina@gmail.com>
2025-09-06 16:22:11 +0300

    fix: responsiveness for deployment

--- client/src/pages/Favorites.tsx
+++ client/src/pages/Favorites.tsx
@@ -5,7 +5,7 @@ const Favorites = () => {
   return (
     <div className="relative min-h-screen w-full flex justify-center items-start overflow-x-hidden">
       <GalaxyScene  /> 
-      <div className="absolute top-1/2 transform -translate-y-1/2 w-10/12 max-w-[85%] py-6">
+      <div className="absolute top-1/4 transform -translate-y-1/2 w-10/12 max-w-[85%] py-3">
         <h1
           className="font-orbitron uppercase text-2xl sm:text-3xl font-bold text-center text-cyan-400 tracking-widest
                      [text-shadow:0_0_3px_#0ff,0_0_3px_#0ff] mb-4"
