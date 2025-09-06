149a89982af800e6a6ad68ad6f0575ce462ba5f8
SashaCHU-st <alexandra.churina@gmail.com>
2025-09-06 18:24:32 +0300

    fix: responsiveness for deployment

--- client/src/pages/MoreDetails.tsx
+++ client/src/pages/MoreDetails.tsx
@@ -5,7 +5,7 @@ const MoreDetails = () => {
   return (
     <div className="relative min-h-screen w-full flex justify-center items-start overflow-x-hidden">
       <Saturn />
-      <div className="absolute top-1/2 right-[40%] transform -translate-y-1/2 w-7/12 max-w-[95%] px-4">
+      <div className="absolute top-1/2  transform -translate-y-1/2  max-w-[95%] px-4">
           <Details />
       </div>
     </div>
