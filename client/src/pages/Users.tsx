7dbf05f5afc428e27cb75c6fd234dddfb521fc59
SashaCHU-st <alexandra.churina@gmail.com>
2025-09-06 16:27:20 +0300

    fix: responsiveness for deployment

--- client/src/pages/Users.tsx
+++ client/src/pages/Users.tsx
@@ -5,7 +5,7 @@ const Users = () => {
   return (
     <div className="relative min-h-screen w-full flex justify-center items-start overflow-x-hidden bg-black">
       <Moon /> 
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10/12 sm:w-8/12 md:w-6/12 max-w-[90%] px-2">
+      <div className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10/12 sm:w-8/12 md:w-6/12 max-w-[90%] px-2">
         <h1
           className="font-orbitron uppercase text-sm sm:text-lg md:text-xl font-bold text-center text-cyan-400 tracking-widest
                      [text-shadow:0_0_2px_#0ff,0_0_2px_#0ff] mb-2"
