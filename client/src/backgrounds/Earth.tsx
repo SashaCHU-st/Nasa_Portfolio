4012fb98bc117008a043f023223b4aae4d5b1065
SashaCHU-st <alexandra.churina@gmail.com>
2025-09-08 19:14:28 +0300

    feat:added my info and changed location of the Earth

--- client/src/backgrounds/Earth.tsx
+++ client/src/backgrounds/Earth.tsx
@@ -55,8 +55,9 @@ const Earth = () => {
       shininess: 15,
     });
     const earth = new THREE.Mesh(earthGeometry, earthMaterial);
-    earth.position.x = -2;
-    earth.position.y = 0.7;
+    earth.position.x = 1.7;
+    earth.position.y = 0.2;
+    earth.position.z = 1.7;
     scene.add(earth);
 
     ///stars
