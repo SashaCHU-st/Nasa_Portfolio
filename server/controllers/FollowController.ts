0144f38de88bd81f0c954d059e1aef415d7a05eb
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-09-11 16:38:21 +0300

    feat:endpoint for unfollow users and check if user already following or not following

--- server/controllers/FollowController.ts
+++ server/controllers/FollowController.ts
@@ -10,9 +10,19 @@ export async function subscribe(data:FollowBody,req: FastifyRequest, reply:Fasti
     const {follow_id} = data;
         console.log("KKKK=>",follow_id)
 
-        const subscride = await pool.query(`INSERT INTO follower (user_id, follow_id) VALUES ($1, $2)`,[userId, follow_id])
+        const checkIfSubscribed  = await pool.query(`SELECT * FROM follower WHERE user_id=$1 AND follow_id = $2 `,[userId, follow_id])
 
-        return reply.code(201).send({message:"Now I am following"})
+        console.log("UUUUU=>", checkIfSubscribed)
+
+        if(checkIfSubscribed.rowCount === 0)
+        {
+            const subscride = await pool.query(`INSERT INTO follower (user_id, follow_id) VALUES ($1, $2)`,[userId, follow_id])
+            return reply.code(201).send({message:"Now I am following"})
+        }
+        else
+        {
+            return reply.code(200).send({message:"Subsribed aready"})
+        }
 
     }catch(err:any)
     {
@@ -20,6 +30,21 @@ export async function subscribe(data:FollowBody,req: FastifyRequest, reply:Fasti
         reply.code(500).send({message:"Something went wrong"})
     }
 }
+
+export async function unsubscribe(data:FollowBody,req: FastifyRequest, reply:FastifyReply) {
+    
+    try{
+        const userId = await authorisation(req, reply);
+        const {follow_id} = data;
+        const deleteSubscride = await pool.query(`DELETE FROM follower WHERE user_id = $1 AND follow_id = $2`,[userId, follow_id])
+        return reply.code(200).send({message:"Deleted from subscrition"})
+    }catch(err:any)
+    {
+        console.error(err.message)
+        reply.code(500).send({message:"Something went wrong"})
+    }
+}
+
 
 
 export async function  mySubscribtions(req: FastifyRequest, reply:FastifyReply) {
--- server/routes/FollowRoutes.ts
+++ server/routes/FollowRoutes.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../utils/verifyJWT";
 import { SubscribeSchema } from "../schema/FollowSchema";
-import { subscribe } from "../controllers/FollowController";
+import { subscribe, unsubscribe } from "../controllers/FollowController";
 import { mySubscribtions } from "../controllers/FollowController";
 
 export async function FollowRoutes(fastify:FastifyInstance) {
@@ -20,6 +20,24 @@ fastify.post("/subscribe", async (req, reply) => {
     };
 
     return subscribe(data, req, reply);
+  }),
+
+
+  fastify.delete("/unsubscribe", async (req, reply) => {
+    if (!(await verifyJWT(req, reply))) return;
+    const validated = SubscribeSchema.safeParse(req.body);
+    if (!validated.success) {
+      const message = validated.error.issues[0]?.message || "Validation failed";
+      reply.code(400).send({ message });
+      return;
+    }
+
+    console.log("mmmm=>",validated.data.follow_id)
+    const data = {
+      follow_id: validated.data.follow_id,
+    };
+
+    return unsubscribe(data, req, reply);
   }),
 
   fastify.get("/getMySubscribe", mySubscribtions);
